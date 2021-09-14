51111333deb385fa7452053087e7cec3a1cf7a45
edelwud <edelwud@icloud.com>
2021-09-14 15:46:37 +0300

    add: giu library for ui

--- client/main.go
+++ client/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	g "github.com/AllenDang/giu"
+)
+
+func onClickMe() {
+	fmt.Println("Hello world!")
+}
+
+func onImSoCute() {
+	fmt.Println("Im sooooooo cute!!")
+}
+
+func loop() {
+	g.SingleWindow().Layout(
+		g.Label("Hello world from giu"),
+		g.Row(
+			g.Button("Click Me").OnClick(onClickMe),
+			g.Button("I'm so cute").OnClick(onImSoCute),
+		),
+	)
+}
 
 func main() {
-	fmt.Println("bl")
+	wnd := g.NewMasterWindow("Hello world", 400, 200, g.MasterWindowFlagsNotResizable)
+	wnd.Run(loop)
 }
