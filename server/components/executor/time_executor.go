9cea0ab0db60f9c04cac7d4fe66a66e6913cabbb
edelwud <edelwud@icloud.com>
2021-09-17 14:39:00 +0300

    add: time executor impl

--- server/components/executor/time_executor.go
+++ server/components/executor/time_executor.go
@@ -2,13 +2,24 @@ package executor
 
 import (
 	"server/components/session"
+	"time"
 )
 
 type TimeExecutor struct {
 	ctx session.SessionStorage
 }
 
-func (e TimeExecutor) Process(remoteAddr string, params ...string) error {
+func (e TimeExecutor) Process(remoteAddr string, _ ...string) error {
+	conn, err := e.ctx.Find(remoteAddr)
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write([]byte(time.Now().String() + "\n"))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
