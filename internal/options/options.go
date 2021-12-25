d6db1c30a728795bc0f90d658eeb4a439719489c
Maksim Yersh <edelwud@icloud.com>
2021-12-26 00:57:23 +0300

    add: protocol support

--- internal/options/options.go
+++ internal/options/options.go
@@ -2,6 +2,7 @@ package options
 
 // Options declares generalized structure of server parameters
 type Options struct {
+	Protocol        string `yaml:"protocol"`
 	Host            string `yaml:"host"`
 	Port            string `yaml:"port"`
 	KeepAlive       bool   `yaml:"keepAlive"`
